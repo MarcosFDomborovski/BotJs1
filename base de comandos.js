4372a1676f47fee4f6d6790a503bb7f7c78a8e46
Marcos F. Domborovski <marcosfelipedomborovski@gmail.com>
2024-07-02 17:59:22 -0300

    slowmode

---
+++ base de comandos.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "",
+    description: "",
+    type: Discord.ApplicationCommandType.ChatInput,
+    options: [
+        {
+            name: "",
+            description: "",
+            type: Discord.ApplicationCommandOptionType.User,
+            required: true,
+        },
+    ],
+
+    run: async (client, interaction) => {
+
+    }
+}
