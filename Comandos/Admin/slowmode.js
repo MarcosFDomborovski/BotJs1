239e25d0dc3630cf49ac0638a63ea5164be92f1b
Marcos F. Domborovski <marcosfelipedomborovski@gmail.com>
2024-07-17 17:54:57 -0300

    slowmode fix

--- Comandos/Admin/slowmode.js
+++ Comandos/Admin/slowmode.js
@@ -1,5 +1,5 @@
-const Discord = require("discord.js")
-const ms = require("ms")
+const Discord = require("discord.js");
+const ms = require("ms");
 
 module.exports = {
     name: "slowmode",
@@ -21,25 +21,36 @@ module.exports = {
     ],
 
     run: async (client, interaction) => {
-
         if (!interaction.member.permissions.has(Discord.PermissionFlagsBits.ManageChannels)) {
-            interaction.reply({ content: `Você não tem permissão para utilizar este comando!`, ephemeral: true })
-        } else {
-            let t = interaction.options.getString("tempo");
-            let tempo = ms(t);
-            let channel = interaction.options.getChannel("canal");
-            if (!channel || channel === null) channel = interaction.channel;
-            if (!tempo || tempo === false || tempo === null) {
-                interaction.reply({ content: `Forneça um tempo válido: [s|m|h].`, ephemeral: true })
-
-            } else {
-                interaction.channel.setRateLimitPerUser(tempo / 1000).then(() => {
-                    interaction.reply({ content: `O canal de texto ${channel} teve seu modo lento definido para \`${t}\`.` })
-                }).catch(() => {
-                    interaction.reply({ content: `Algo deu errado ao executar este comando, verifique minhas permissões`, ephemeral: true })
-                })
-            }
+            return interaction.reply({ content: `Você não tem permissão para utilizar este comando!`, ephemeral: true });
         }
 
+        let t = interaction.options.getString("tempo");
+        let tempo = ms(t);
+        let channel = interaction.options.getChannel("canal") || interaction.channel;
+
+        if (!tempo && t !== "0s") {
+            return interaction.reply({ content: `Forneça um tempo válido: [s|m|h].`, ephemeral: true });
+        }
+
+        if (t === "0s") {
+            if (channel.rateLimitPerUser === 0) {
+                return interaction.reply({ content: `O modo lento já está desativado no canal ${channel}.`, ephemeral: true });
+            }
+
+            channel.setRateLimitPerUser(0).then(() => {
+                interaction.reply({ content: `O modo lento foi desativado no canal ${channel}.` });
+            }).catch(e => {
+                interaction.reply({ content: "Algo deu errado ao desativar o modo lento.", ephemeral: true });
+                console.error(e);
+            });
+        } else {
+            channel.setRateLimitPerUser(tempo / 1000).then(() => {
+                interaction.reply({ content: `O canal de texto ${channel} teve seu modo lento definido para \`${t}\`.` });
+            }).catch(e => {
+                interaction.reply({ content: `Algo deu errado ao executar este comando, verifique minhas permissões.`, ephemeral: true });
+                console.error(e);
+            });
+        }
     }
-}
+};
