da88f1784d0ad958b455eab5b59d5476811cac31
d4rknorris <marcosfelipedomborovski@gmail.com>
2024-07-17 22:38:01 -0300

    status msg

--- Events/Status.js
+++ Events/Status.js
@@ -5,6 +5,7 @@ const client = require("../index");
 const DiscordRPC = require("discord-rpc");
 const RPC = new DiscordRPC.Client({ transport: "ipc" });
 const ID = "1243649637903110227"; // ID da aplicação do discord.
+
 DiscordRPC.register(ID);
 
 async function activity() {
@@ -12,23 +13,20 @@ async function activity() {
   RPC.setActivity({
     details: "De boa",
     state: "Esperando virar o mês 💸",
-    largeImageKey:
-      "https://cdn.discordapp.com/attachments/1106736905313337397/1263300426527281254/faf.png?ex=6699bbb4&is=66986a34&hm=33a01a519b931e3ff578d0f243858f6099b88c208e524db8bcb7ad7d74e08414&",
-    largeImageText: "sim",
     smallImageKey:
       "https://cdn.discordapp.com/avatars/474334792830156805/336c45ee65b6a5ce56896dbb1b2c539d.webp?size=1024&format=webp&width=0&height=192",
-    smallImageText: "textin",
+    smallImageText: "PIMPOSO",
 
     instance: false,
     startTimestamp: Date.now(),
     buttons: [
       {
         label: `YouTube`,
-        url: `https://www.youtube.com/watch?v=jtIYMTL51ok`,
+        url: `https://www.youtube.com/watch?v=dQw4w9WgXcQ&pp=ygUIcmlja3JvbGw%3D`,
       },
       {
-        label: `Google`,
-        url: `https://www.google.com`,
+        label: `Twitch`,
+        url: `https://www.twitch.tv/d4rknorris`,
       },
     ],
   });
